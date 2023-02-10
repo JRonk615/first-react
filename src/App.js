14707d0e710603c7af4479133bd0265c528ff4a8
Jordan Ronk <ronkj74@gmail.com>
2023-02-09 20:12:43 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className='my-class'> Hello Dojo!</h1>
+    <h2> Things I need to do: </h2>
+
+    <ul>
+      <li>Learn React </li>
+      <li>Learn React </li>
+      <li>Learn React </li>
+      <li>Learn React </li>
+    </ul>
+
+    </>
+    
+    
+    );
+  }
+
+
+export default App;
